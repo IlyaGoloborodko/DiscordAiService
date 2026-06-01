1053aee597beaa928197e11d6301e6251f9a634d
DrGen <sok203333@gmail.com>
2026-06-01 21:26:09 +0300

    change tts model + add retries on text model. Add readme

--- app/services/prompt_service.py
+++ app/services/prompt_service.py
@@ -6,12 +6,12 @@ from pydantic_ai.models.openai import OpenAIChatModel
 from pydantic_ai.providers.openai import OpenAIProvider
 
 from app.data.models import PromptInfo, MusicAgentOutput
-from .tts_service import text_to_speech
 
 class PromptService:
     def __init__(self):
         self.tm_base_url = os.getenv("TM_BASE_URL")
         self.tm_api_key = os.getenv("TM_API_KEY")
+        self.model = os.getenv("TM_MODEL_NAME")
 
     def _provider(self) -> OpenAIProvider:
         return OpenAIProvider(
@@ -21,16 +21,19 @@ class PromptService:
 
     async def get_text_response(self, prompt_info: PromptInfo) -> AgentRunResult[Any]:
         model = OpenAIChatModel(
-            model_name="google/gemma-3-4b",
+            model_name=self.model,
             provider=self._provider(),
         )
-        agent = Agent(model)
+        agent = Agent(
+            model,
+            retries=5,
+        )
         question = f'{prompt_info.user_name or "Unknown user"} asks: {prompt_info.user_message}'
         return await agent.run(question)
 
     async def get_agent_response_with_search_str(self, prompt_info: PromptInfo) -> AgentRunResult[Any]:
         model = OpenAIChatModel(
-            model_name="google/gemma-3-4b",
+            model_name=self.model,
             provider=self._provider(),
         )
         agent = Agent(
@@ -40,9 +43,3 @@ class PromptService:
         )
         question = f'{prompt_info.user_name or "Unknown user"} asks: {prompt_info.user_message}'
         return await agent.run(question)
-
-    async def process_prompt(self, prompt_info: PromptInfo):
-        agent_response = await self.get_text_response(prompt_info)
-
-        async for chunk in text_to_speech(agent_response.output):
-            yield chunk
